5bcb6328f100f42ea58fad514b77a77830cd2309
ekuin0x <76911469+ekuin0x@users.noreply.github.com>
2024-01-08 07:59:09 +0100

    Delete pull.py

--- pull.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-while True :
-    res = requests.get("https://raw.githubusercontent.com/zloi-user/hideip.me/main/http.txt")
-    with open("proxies.txt", 'w', encoding='utf-8') as f :
-        f.write(res.text)
-    print("PROXIES REFRESHED")
-    time.sleep(300)
